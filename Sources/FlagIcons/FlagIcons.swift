c31ac9ac5abedb16ea19479e496ec922665b35ee
László Tuss <2952613+laszlotuss@users.noreply.github.com>
2022-05-08 18:50:25 +0200

    Refactored Source structure to prepare for SPM

---
+++ Sources/FlagIcons/FlagIcons.swift
@@ -0,0 +1,97 @@
+//
+//  FlagIcons.swift
+//  LastDay
+//
+//  Created by Mateusz Malczak on 17/09/16.
+//  Copyright © 2016 The Pirate Cat. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+/**
+ Represents a flags icon sprite sheet
+ */
+open class FlagIcons {
+    
+    public struct Country {
+        public let name: String;
+        public let code: String;
+    }
+    
+    open class func loadDefault() -> SpriteSheet? {
+        guard let assetsBundle = assetsBundle() else {
+            return nil
+        }
+        
+        if let infoFile = assetsBundle.path(forResource: "flags", ofType: "json") {
+            return self.loadSheetFrom(infoFile)
+        }
+        
+        return nil
+    }
+    
+    open class func loadCountries() -> [Country]? {
+        guard let assetsBundle = assetsBundle() else {
+            return nil
+        }
+        
+        if let dataFile = assetsBundle.path(forResource: "countries", ofType: "json") {
+            if let countriesSet: [[String:String]] = loadJSON(file: dataFile) {
+                return countriesSet.compactMap({countryInfo in
+                    guard case let (name?, code?) = (countryInfo["name"], countryInfo["code"]) else {
+                        return nil
+                    }
+                    return Country(name: name, code: code)
+                }).sorted(by: {country1, country2 in country1.name < country2.name})
+            }
+        }
+        
+        return nil
+    }
+    
+    open class func loadSheetFrom(_ file: String) -> SpriteSheet? {
+        guard let assetsBundle = assetsBundle() else {
+            return nil
+        }
+        
+        if let infoObj: [String:Any] = loadJSON(file: file) {
+            if let gridSizeObj = infoObj["gridSize"] as? [String:Int],
+                let spriteSizeObj = infoObj["spriteSize"] as? [String:Int] {
+                let gridSize = (gridSizeObj["cols"]!, gridSizeObj["rows"]!)
+                let spriteSize = (spriteSizeObj["width"]!, spriteSizeObj["height"]!)
+                
+                if let codes = (infoObj["codes"] as? String)?.components(separatedBy: "|") {
+                    if let sheetFileName = infoObj["sheetFile"] as? String,
+                        let resourceUrl = assetsBundle.resourceURL {
+                        let sheetFileUrl = resourceUrl.appendingPathComponent(sheetFileName)
+                        if let image = UIImage(contentsOfFile: sheetFileUrl.path) {
+                            let info = SpriteSheet.SheetInfo(gridSize: gridSize, spriteSize: spriteSize, codes: codes)
+                            return SpriteSheet(sheetImage: image, info:  info)
+                        }
+                    }
+                }
+            }
+        }
+        
+        return nil
+    }
+    
+    fileprivate class func loadJSON<T>(file: String) -> T? {
+        guard let data = try? Data(contentsOf: URL(fileURLWithPath: file)) else {
+            return nil
+        }
+        
+        let options = JSONSerialization.ReadingOptions(rawValue: 0)
+        return try? JSONSerialization.jsonObject(with: data, options: options) as? T
+    }
+
+    fileprivate class func assetsBundle() -> Bundle? {
+        let bundle = Bundle(for: self)
+        guard let assetsBundlePath = bundle.path(forResource: "assets", ofType: "bundle") else {
+            return nil
+        }
+        return Bundle(path: assetsBundlePath);
+    }
+    
+}
--- flag-icon-swift.podspec
+++ flag-icon-swift.podspec
@@ -5,14 +5,13 @@ s.version      = "0.1.0"
 s.license      = "MIT"
 s.homepage     = "https://github.com/malczak/flag-icon-swift"
 s.summary      = "A collection of all country flags to be used in Swift"
-s.author       = { "Mateusz Malczak" => "mateusz@malczak.info" }
+s.author       = { "Mateusz Malczak" => "mateusz@malczak.info", "Laszlo Tuss" => "laszlotuss@me.com" }
 s.source       = { :git => "https://github.com/malczak/flag-icon-swift.git", :branch => "swift" }
 
 s.platform     = :ios, "8.0"
 
-s.source_files  = "*.swift"
-s.exclude_files = "*Tests.swift"
-s.resource_bundle = { 'assets' => 'flag-assets/*'}
+s.source_files  = "Sources/FlagIcons/*.swift"
+s.resource_bundle = { 'assets' => 'Sources/FlagIcons/flag-assets/*'}
 
 s.requires_arc = true
 end
